ff4f85d6d987bbcf8a2ef5adbcab4e8a5b80f97f
bitcoindoubler <bitcoindoubler1@gmail.com>
2016-02-23 14:29:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var config = {
   app_name: 'Bitcoin Doubler',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
+  recaptcha_sitekey: '6Lfs_xgTAAAAAGJDODatb3OVIbP9oFxzJH18fb3i',  // <----- EDIT ME!
   redirect_uri: 'https://bitcoindoubler.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
